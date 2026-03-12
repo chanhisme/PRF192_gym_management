bc1530a61b2bebe5bdc1282bf3c883f386c80846
chanhisme <chanhle1907@gmail.com>
2026-03-12 19:59:08 +0700

    Delete Final_project_PRF192 directory

--- Final_project_PRF192/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-int canRemove(struct memberProfile *members, int index);
-int isValidId(char id[]);
-int worldCouting(char *name);
-int isValidName(char *name);
-int isValidBirthYear(int year);
-int isValidMemberShipType(int choice);
-#endif
